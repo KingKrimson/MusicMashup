69b9c6f23f8fd36a064e104ccabe46b608cf5112
Andrew Brown <Andrew@brownfamilymail.co.uk>
2013-02-03 01:54:37 +0000

    First commit.
    There's nothing really here yet; just the skeleton of the site. Give it
    another week, and it should start to look like a proper website.

---
+++ database.php
@@ -0,0 +1,6 @@
+<?php
+$db_hostname = 'localhost';
+$db_database = 'music_mashup_database';
+$db_username = 'root';
+$db_password = 'password';
+?>
---
+++ home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="./CSS/style.css">
+        <title>Alternews - Home</title>
+    </head>
+    <body>
+        <?php include_once 'header.html';?>
+
+        <?php include_once 'footer.html'; ?>    
+    </body>
+
+
+</html>
