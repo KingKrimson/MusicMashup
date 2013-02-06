1acee6588a300d67bfd15cdcd4d3778b0179e306
Andrew Brown <Andrew@brownfamilymail.co.uk>
2013-02-06 02:42:42 +0000

    Eighth commit
    I lied. I've found a (hacky?) workaround for the issue I was having
    before; it involves having a hidden input field which has a certain
    number as a value. This value is checked when the file is loaded, and
    the appropriate function is called. Currently the final step of the
    registration process is buggy. Not sure why at the moment; it seems to
    think a variable I send to it via POST doesn't exist, and my syntax for
    the 'UPDATE' SQL query is wrong.

--- registration.php
+++ registration.php
@@ -6,10 +6,12 @@
      favouriting artists and tracks, and add some simple information about
      themselves to the database.                                             -->
 <?php
-if (!$_COOKIE) {
+if (!$_COOKIE && !$_POST) {
     firstLoad();
-} else if(!$_POST) {
+} else if ($_POST["num"] == 1) {
     extraDetails();
+} else if ($_POST["num"] == 2) {
+    registrationComplete();
 } else {
     alreadyRegistered();
 }
@@ -33,10 +35,11 @@ _END;
             <h1>Register</h1>
             <p>Start your journey here! Sign up, and dive into the world of alternate rock.</p>
             <div class=register> <!-- start of login -->
-                <form class="register" action="" method="post">
+                <form class="register" action="registration.php" method="post">
                     <table class="register">
                         <tr><td><input type="text" id="Name" name="username" style="color:#9E9E9E; font-style:italic" value="Desired Username" size="16"/></td></tr>
                         <tr><td><input type="text" id="Password" name="password" style="color:#9E9E9E; font-style:italic" value="Desired Password" size="16"/></td></tr>
+                        <tr><td><input type="hidden" name="num" value="1"/></td></tr>
                         <tr><td><input type="submit" value="Register" /></td></tr>
                     </table>
                 </form>
@@ -62,10 +65,11 @@ function extraDetails() {
     </head>
     <body>
 _END;
-    include_once 'header';
+    include_once 'header.html';
     echo '<div class="clear />';
 
     echo'<div id="pagecontent">';
+    include 'databasevars.php';
     $db_server = mysql_connect($db_hostname, $db_username, $db_password);
     if (!$db_server) {
         echo '<p>Could not connect to database:' . mysql_error() . '</p>';
@@ -79,19 +83,21 @@ _END;
         <p>Congratulations {$_POST['username']}! You have successfully registered!
             Why not tell us a bit about yourself?</p>
         <div class=register> <!-- start of login -->
-            <form class="register" action="" method="post">
+            <form class="register" action="registration.php" method="post">
                 <table class="register">
                     <tr><td><input type="text" id="Realname" name="realname" style="color:#9E9E9E; font-style:italic" value="" size="16"/></td></tr>
                     <tr><td><input type="number" id="Age" name="age" style="color:#9E9E9E; font-style:italic" value="" size="16"/></td></tr>
                     <tr><td><input type="text" id="Description" name="description"></tr></td>
                     <tr><td><input type="hidden" id="Username name="username" value="{$_POST['username']}"></tr></td>
+                    <tr><td><input type="hidden" name="num" value="2"/></td></tr>
                     <tr><td><input type="submit" value="Register" /></td></tr>
                 </table>
             </form>
         </div>  <!-- End of login --> 
 _END;
+            mysql_close();
             echo "<div class='clear'>";
-            include 'widgetpane.html';
+            include 'widgetpane.php';
             include 'footer.html';
             echo '  </body>';
             echo '</html>';
@@ -111,24 +117,26 @@ function registrationComplete() {
     </head>
     <body>
 _END;
-    include_once 'header';
+    include_once 'header.html';
     echo '<div class="clear" />';
     echo '<div id="pagecontent">';
+    include 'databasevars.php';
     $db_server = mysql_connect($db_hostname, $db_username, $db_password);
     if (!$db_server) {
         echo '<p>Could not connect to database:' . mysql_error() . '</p>';
     } else {
         $database = mysql_select_db($db_database);
-        $insertuserquery = "UPDATE user SET realname='$_POST[realname]', age='$_POST[age]', description='$_POST[description]' WHERE username='$_POST[username]";
+        $insertuserquery = "UPDATE user SET realname='$_POST[realname]', age='$_POST[age]', description='$_POST[description]' WHERE username='$_POST[username]'";
         if (!mysql_query($insertuserquery)) {
             echo "Couldn't insert your data into database: " . mysql_error();
         } else {
             echo "<p>looks like you\'re all set, {$_POST['username']}! Go favourite some tracks!</p>";
         }
+        mysql_close();
         echo '</div>';
     }
     echo '<div class="clear"/>';
-    include 'widgetpane.html';
+    include 'widgetpane.php';
     include 'footer.html';
     echo '  </body>';
     echo '</html>';
